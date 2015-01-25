8eed9dc51ef36e11485cc048848de3a474917f87
charlenni <web@weltz-online.de>
2015-01-25 11:12:25 +0100

    Update Lua library to Arm64

--- Eluant/iOS/AssemblyInfo.cs
+++ Eluant/iOS/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using ObjCRuntime;
 using System.Reflection;
 
 [assembly: CLSCompliantAttribute (true)]
-[assembly: LinkWith("liblua5.1.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
+[assembly: LinkWith("liblua5.1.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
